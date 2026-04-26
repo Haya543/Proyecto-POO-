4eaa46c8d039ef3f2618b6eafd3022492a758b93
ioSoyPato <patrikwars@gmail.com>
2026-04-25 21:24:03 -0600

    feat: implement Super, Ultra, and Master sphere variants with unique capture rates and mechanics

---
+++ backend/src/main/java/juego/esferas/UltraEsfera.java
@@ -0,0 +1,8 @@
+package juego.esferas;
+
+// 2x capture rate, most reliable sphere available in shops
+public class UltraEsfera extends PokeEsfera {
+    public UltraEsfera() {
+        super("Ultra Esfera", 2.0);
+    }
+}
